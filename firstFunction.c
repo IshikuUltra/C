b076e0c93eebdb701ef79bba8e673b63f7bd5945
Sam <mason.rad@tutanota.com>
2020-10-18 19:12:42 -0500

    end of day

--- firstFunction.c
+++ firstFunction.c
@@ -3,17 +3,19 @@
 float average(int length, int array[]);
 int main(void) 
 {
-    int s;
-    int f;
+    int s = 0;// when you set a variable by int s; s=3032923
+    int f = 0;//same here.
+
     printf("Enter number of scores: ");
-    int n = scanf("%d", &f);
+    int n = scanf("%d", &f); //why does n have a val=600k
+
     int scores[n]; // dynamic variable assignment
     for (int i=0; i < n-1; i++){ // if i hard code 3 then program runs 3 times, why no work dynamically with n?
         scores[i] = printf("Enter a score: "); 
         scanf("%d", &s); //why does program not stop asking for input after n loops?
     };
     printf("Average: %f.2", average(n, scores));
-    return 0; //might need to take this out
+    return 0; // 0=no error, 1=error./
 }
 
 float average(int length, int array[])
