8a7558b34b7362441776333ca786a6a0ae6669a8
Sam Goodnight <mason.rad@tutanota.com>
2020-12-28 14:37:29 -0600

    'first'

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(void)
+int main(void)//set breakpoint
 {
+    int rs = 0;//add rs to watch list
     printf("Hello, world!!\n");
+    rs = 9;//be amazed
     return 0;
 }
